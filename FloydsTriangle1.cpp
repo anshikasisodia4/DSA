4d70675f32c86c0810fab95f1db5a0faee1c46fd
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-02 12:55:42 +0530

    Initial commit

--- FloydsTriangle1.cpp
+++ FloydsTriangle1.cpp
@@ -5,10 +5,10 @@ int main()
 {
     int n;
     cin >> n;
-int num=1;
+    int num = 1;
     for (int i = 0; i < n; i++)
     {
-        for (int j =0; j <i+1; j++)
+        for (int j = 0; j < i + 1; j++)
         {
             cout << num << " ";
             num++;
