f928cd5e874451466e2e4aee07a103d61b3ddbd7
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-02 14:46:20 +0530

    Initial commit

--- InvertedPyramid.cpp
+++ InvertedPyramid.cpp
@@ -5,15 +5,20 @@ int main()
 {
     int n;
     cin >> n;
-    char ch='A';
+    int num = 1;
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 0; j < i; j++)
         {
-            cout << ch << " ";
-            ch++;
+            cout << " ";
         }
+        for (int j = 0; j < n - i; j++)
+        {
+            cout << (i + 1)<<" " ;
+        }
+
         cout << endl;
     }
+
     return 0;
 }
