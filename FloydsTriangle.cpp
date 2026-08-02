affcf0a099608d43a5a6d02ddf631179de09efd4
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-02 12:54:21 +0530

    Initial commit

--- FloydsTriangle.cpp
+++ FloydsTriangle.cpp
@@ -5,13 +5,11 @@ int main()
 {
     int n;
     cin >> n;
-
+int num=1;
     for (int i = 0; i < n; i++)
     {
-        int num = 1;
-        for (int j = 0; j < i + 1; j++)
+        for (int j =0; j <i+1; j++)
         {
-
             cout << num << " ";
             num++;
         }
--- ReverseTriangle1.cpp
+++ ReverseTriangle1.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
+    cin >> n;
 
-    for(int i=0;i<n;i++){
-        int num=1;
-        for(int j=0;j<i+1;j++){
-          
-            cout<<num<<" ";
-            num++;
-
+   
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i+1; j >0; j--)
+        {
+            cout << j << " ";
+           
         }
-        cout<<endl;
+        cout << endl;
     }
+
     return 0;
 }
