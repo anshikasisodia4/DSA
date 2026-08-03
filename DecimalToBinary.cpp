3aa5b2aeea1329907ff0d3ffb334f871520dd950
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-03 06:33:38 +0530

    Initial commit

--- DecimalToBinary.cpp
+++ DecimalToBinary.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+ int decimalToBinary(int num){
+    int ans=0,pow=1;
+
+    while(num>0){
+        int rem=num%2;
+        num=num/2;
+        ans=ans+(rem*pow);
+        pow=pow*10;
+    }
+    return ans;
+ }
+ 
 int main(){
+    int num;
+    cin>>num;
+    cout<<decimalToBinary(num)<<endl;
+
+    // for(int i=0;i<=10;i++){
+    //     cout<<decimalToBinary(i)<<endl;
+    // }
     
     return 0;
 }
