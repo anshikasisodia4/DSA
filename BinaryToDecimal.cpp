9164f6a1e897c099c55c347b9b184e5ee55ae071
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-03 06:49:32 +0530

    Initial commit

--- BinaryToDecimal.cpp
+++ BinaryToDecimal.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int BinaryToDecimal(int n){
+     int pow = 1, ans = 0;
+
+    while(n > 0){
+       int rem = n%10;
+        ans = ans + (rem * pow);
+
+        n /= 10;
+        pow = pow*2;
+    }
+    return ans;
+}
+
 int main(){
+    int n;
+    cin >> n;
+    
+    cout << BinaryToDecimal(n) << endl;
     
     return 0;
 }
