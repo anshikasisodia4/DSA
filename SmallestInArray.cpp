ca3c77d0ae36367e6daabde18fde54326dd67450
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-04 06:29:22 +0530

    Initial commit

--- SmallestInArray.cpp
+++ SmallestInArray.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include<climits>
-#include<algorithm>
+#include <climits>
+// #include <algorithm>
 using namespace std;
 
 int main()
 {
-    int nums[] = {5, 15, 22, 1, -15, -24};
+    int nums[] = {5, 15, 22, 1, -15, 24};
     int size = 6;
 
     int smallest = INT_MAX;
+    int smallestIndex = -1;
+
     for (int i = 0; i < size; i++)
     {
-        smallest = min(nums[i], smallest);
+        // smallest = min(nums[i], smallest);
+        if (smallest > nums[i])
+        {
+            smallest = nums[i];
+            smallestIndex = i;
+        }
     }
     cout << "Smallest is " << smallest << endl;
+    cout << "Index of smallest element is " << smallestIndex << endl;
 
     return 0;
 }
