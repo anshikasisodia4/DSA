c1e5242b2a794fd0f90662cc6b4c7d7d4cf90fe0
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-04 06:29:22 +0530

    Initial commit

--- LargestInArray.cpp
+++ LargestInArray.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <climits>
-#include <algorithm>
+
 using namespace std;
 
 int main()
@@ -9,11 +9,18 @@ int main()
     int size = 5;
 
     int largest = INT_MIN;
+    int largestIndex = -1;
+
     for (int i = 0; i < size; i++)
     {
-        largest = max(arr[i], largest);
+        if (largest < arr[i])
+        {
+            largest = arr[i];
+            largestIndex = i;
+        }
     }
     cout << "Largest value is " << largest << endl;
+    cout << "Index of largest value is " << largestIndex << endl;
 
     return 0;
 }
