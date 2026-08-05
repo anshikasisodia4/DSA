4b8155ef7b7d97c93b4ee8a250ff6b67cd7cb08b
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-05 06:47:26 +0530

    Initial commit

--- ArrUniqueElement.cpp
+++ ArrUniqueElement.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int arr[] = {2, 3, 4, 2, 5, 3, 6, 7, 6};
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "unique elements : " << endl;
+
+    for (int i = 0; i < size; i++)
+    {
+        bool isDuplicate = false;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+
+    cout << endl
+         << "element occring once : " << endl;
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            cout << arr[i] << " ";
+        }
+    }
     return 0;
 }
---
+++ ArrayIntersection.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr1[]={2,4,6,8,9};
+    int arr2[]={1,2,3,4,5};
+
+  int size1= sizeof(arr1)/sizeof (arr1[0]);
+  int size2= sizeof(arr2)/sizeof (arr2[0]);
+
+  cout<<"Intersection "<<endl;
+  for(int i=0;i<size1;i++){
+    for(int j=0;j<size2;j++){
+        if(arr1[i]==arr2[i]){
+cout<<arr1[i]
+        }
+    }
+  }
+
+    return 0;
+}
