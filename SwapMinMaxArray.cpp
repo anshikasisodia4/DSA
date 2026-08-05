e7212103790aa6cfb47dc5ee60d406822082305c
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-05 06:24:35 +0530

    Initial commit

--- SwapMinMaxArray.cpp
+++ SwapMinMaxArray.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+
 using namespace std;
 
-int main(){
-    
+int main()
+{
+    int arr[] = {1, 6, 2, 7, 8};
+    int size = 5;
+
+    int minIndex = 0, maxIndex = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < arr[minIndex])
+        {
+            minIndex = i;
+        }
+        if (arr[i] > arr[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[maxIndex];
+   arr[ maxIndex] = temp;
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
