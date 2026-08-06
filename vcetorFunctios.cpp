2dd83ade7da1550a0063e2caa1bd22546d93afff
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-06 12:39:05 +0530

    Initial commit

--- vcetorFunctios.cpp
+++ vcetorFunctios.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-int main(){
-    vector <char> vec={'a','b','c','d','e'};
-    cout<<"size : "<<vec.size()<<endl;
-        return 0;
+int main()
+{
+    vector<int> vec = {1, 2, 3, 4, 5};
+    cout << "size : " << vec.size() << endl;
+
+    vec.push_back(25);
+    vec.push_back(45);
+    cout << "size after push back : " << vec.size() << endl;
+
+    for (int val : vec)
+    {
+        cout << val << " ";
+    }
+
+    vec.pop_back();
+    cout << endl
+         << "size after pop back : " << vec.size() << endl;
+    cout << "front value : " << vec.front() << endl;
+    cout << "last value : " << vec.back()<<endl;
+
+    cout << "vector at 4 index : " << vec.at(4) << endl;
+    return 0;
 }
