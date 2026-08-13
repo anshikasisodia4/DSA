c2105cf3023b036bed8f479f39c92b4c5308659a
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-13 07:12:07 +0530

    Initial commit

---
+++ ReverseString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+
+    //    for(int i=str.length()-1;i>=0;i--){
+    //     cout<<str[i];
+    //    } cout<<endl;
+    reverse(str.begin(), str.end());
+    cout<<str<<endl;
+    
+    return 0;
+}
