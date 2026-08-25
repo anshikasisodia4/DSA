03452f8c5cc0a8679d1a963676b3005a82abec0a
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-25 06:38:22 +0530

    initial commit

--- PairSumOptimal.cpp
+++ PairSumOptimal.cpp
@@ -1,8 +1,40 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
 
-int main(){
+vector<int> pairSum(vector<int> num, int target)
+{
+    vector<int> ans;
+    int n = num.size();
+    int i = 0, j = n - 1;
+    while (i < j)
+    {
+        int pairSum = num[i] + num[j];
+        if (pairSum > target)
+        {
+            j--;
+        }
+        else if (pairSum < target)
+        {
+            i++;
+        }
+        else
+        {
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> num = {2, 7, 11, 15};
+    int target = 13;
     
+    vector<int> ans = pairSum(num, target);
+    cout << ans[0] << " , " << ans[1] << endl;
+
     return 0;
 }
