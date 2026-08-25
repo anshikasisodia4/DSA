c75ba31b0657bf9b97215531c1bcacd88c630572
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-25 20:24:39 +0530

    initial commit

---
+++ MajorityElementBrute.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int majorityElement(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+
+        if (count > n / 2)
+        {
+            return arr[i];
+        }
+    }
+
+    return -1; 
+}
+
+int main()
+{
+    int arr[] = {2, 2, 1, 1, 1, 2, 2};
+    int n = 7;
+
+    cout << "Majority Element = " << majorityElement(arr, n);
+
+    return 0;
+}
