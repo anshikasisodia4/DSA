efc0eca42c85f91f59d5e764e8057258abea0676
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-26 07:10:41 +0530

    initial commit

--- TrianglePattern1.cpp
+++ TrianglePattern1.cpp
@@ -10,8 +10,7 @@ int main()
     {
         for (int j = 0; j < i + 1; j++)
         {
-            cout << "*"<<" ";
-            
+            cout << "*" << " ";
         }
         cout << endl;
     }
