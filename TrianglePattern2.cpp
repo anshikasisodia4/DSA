5e77bdafb1a9f74ec7df32ebe690a54d73dde567
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-08-27 06:40:52 +0530

    initial commit

--- TrianglePattern2.cpp
+++ TrianglePattern2.cpp
@@ -6,11 +6,11 @@ int main()
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < i + 1; j++)
+        for (int j = 1; j < i + 1; j++)
         {
-            cout << i+1<<" ";
+            cout << i<<" ";
             
         }
         cout << endl;
