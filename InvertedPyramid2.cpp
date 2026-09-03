70da1ce2614b040a838a6cc0d3a36374f9de9010
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-03 06:08:20 +0530

    commit

--- InvertedPyramid2.cpp
+++ InvertedPyramid2.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i;j++){
-            cout<<"*";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    { // space
+        for (int j = 0; j < i; j++)
+        {
+            cout << " ";
         }
-        cout<<endl;
+        // star
+        for (int j = 0; j < 2 * n - (2 * i + 1); j++)
+        {
+            cout << "*";
+        }
+        // adding or removing this does not change the output
+        // space
+        // for (int j = 0; j < n; j++)
+        // {
+        //     cout << " ";
+        // }
+        cout << endl;
     }
-    
+
     return 0;
 }
