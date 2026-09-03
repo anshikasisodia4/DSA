630481b2c26073156dbfb91598925978a0a24107
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-03 06:44:13 +0530

    commit

--- Diamnod.cpp
+++ Diamnod.cpp
@@ -22,9 +22,13 @@ int main()
     for (int i = n - 2; i >= 0; i--)
     {
         for (int j = 0; j < n - i - 1; j++)
+        {
             cout << " ";
+        }
         for (int j = 0; j < 2 * i + 1; j++)
+        {
             cout << "*";
+        }
         cout << endl;
     }
 
