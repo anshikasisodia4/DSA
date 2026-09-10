309cbe77f375964ebdb6f065f0646d1fad0fbb18
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-10 22:23:57 +0530

    commit

--- Recursion8.cpp
+++ Recursion8.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    
+void reverseArr(int arr[], int left, int right)
+{
+    if (left >= right)
+    {
+        return;
+    }
+    else
+    {
+        swap(arr[left], arr[right]);
+        reverseArr(arr, left + 1, right - 1);
+    }
+}
+
+int main()
+{
+    int arr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>> arr[i];
+    }
+
+    reverseArr(arr, 0, 4);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
