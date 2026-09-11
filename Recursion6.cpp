44bddf1a65a70f6b937163efd527017fcaca554c
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-11 07:23:08 +0530

    commit

--- Recursion6.cpp
+++ Recursion6.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int f(int n){
+int sum(int n){
     if(n==0){
         return 0;
     }
-    return n+f(n-1);
+    return n+sum(n-1);
 }
 
 int main(){
     int n;
     cin>>n;
 
-    cout<<f(n);
+    cout<<sum(n);
     return 0;
 
 }
