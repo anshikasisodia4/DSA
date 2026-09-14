67b2d2e1f0d1a17ddd5bdf554147d9b4bd3329dc
anshikasisodia4 <anshikasisodiya81@gmail.com>
2026-09-14 06:40:25 +0530

    commit

--- Hashing1.cpp
+++ Hashing1.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
-int main(){
-      
+int main()
+{
+
     int n;
-    cin>>n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // precompute
+    int hash[13]={0};
+    for(int i=0;i<n;i++){
+        hash[arr[i]]+=1;
+
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        int number;
+        cin >> number;
+        // fetch
+        cout<<hash[number]<<endl;
+    }
 
     return 0;
 }
